675b3faa5a885a0929d5273246ab604169bfa5d8
Drake Scott <dascott0204@gmail.com>
2017-03-05 12:04:47 -0600

    setup my first couple of routes with vue-router.

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+/**
+ * Created by dasco on 3/5/2017.
+ */
+import User from './components/user/User.vue';
+import Home from './components/Home.vue';
+
+export const routes = [
+    {path: '/', component: Home},
+    {path: '/user', component: User}
+];
