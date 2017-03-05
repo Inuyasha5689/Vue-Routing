db2e3e019376204e58d9d1938392b0b1eddd73c9
Drake Scott <dascott0204@gmail.com>
2017-03-05 12:21:46 -0600

    styled active links

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <ul class="nav nav-pills">
+        <router-link to="/" tag="li" activeClass="active" exact=""><a>Home</a></router-link>
+        <router-link to="/user" tag="li" activeClass="active"><a>Profile</a></router-link>
+
+    </ul>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
