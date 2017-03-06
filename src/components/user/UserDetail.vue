241cad7277cfbeabbb5584e570454edd7a7095d8
Drake Scott <dascott0204@gmail.com>
2017-03-05 18:16:23 -0600

    setup a transition for my router view

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -2,7 +2,7 @@
     <div>
         <h3>Some User Details</h3>
         <p>User loaded has ID: {{ $route.params.id }}</p>
-        <router-link tag="button" :to="{ 'userEdit', params: { $route.params.id }, { 'en', q:100 } }"
+        <router-link tag="button" :to="{ name: 'userEdit', params: { id: $route.params.id }, query: { locale: 'en', q:100 } }"
                      class="btn btn-primary">Edit User
         </router-link>
     </div>
